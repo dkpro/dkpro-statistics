a9da90b821adc33239be7f8fb04e08fb6c7ee410
roland.kluge.de@googlemail.com <roland.kluge.de@googlemail.com@f11ac0c2-8a94-b94a-46a3-c99d466b71ef>
2013-12-06 10:36:00 +0000

    Add failing test for perfect expected agreement
    Test case is commented out.

--- trunk/de.tudarmstadt.ukp.statistics/src/test/java/de/tudarmstadt/ukp/dkpro/statistics/agreement/TwoRaterAgreementTest.java
+++ trunk/de.tudarmstadt.ukp.statistics/src/test/java/de/tudarmstadt/ukp/dkpro/statistics/agreement/TwoRaterAgreementTest.java
@@ -17,13 +17,6 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.statistics.agreement;
 
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.AnnotationStudy;
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.IAnnotationStudy;
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.TwoRaterKappaAgreement;
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.TwoRaterObservedAgreement;
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.TwoRaterObservedCategoryAgreement;
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.TwoRaterPiAgreement;
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.TwoRaterSAgreement;
 import junit.framework.TestCase;
 
 /**
@@ -32,139 +25,164 @@ import junit.framework.TestCase;
  * @author Christian M. Meyer
  * @date 04.11.2009
  */
-public class TwoRaterAgreementTest extends TestCase {
+public class TwoRaterAgreementTest
+    extends TestCase
+{
 
-	/***/
-	public void testArtsteinPoesio2008_1() {
-		IAnnotationStudy study = AgreementTestExamples.createArtsteinPoesio2008_1();
-		
-		// Two raters, observed agreement.
-		TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
-		assertEquals(0.7, raw.calculateAgreement(), 0.001);
-		
-		TwoRaterObservedCategoryAgreement cat = new TwoRaterObservedCategoryAgreement(study);
-		assertEquals(0.571, cat.calculateAgreement("STAT"), 0.001);
-		assertEquals(0.769, cat.calculateAgreement("IReq"), 0.001);
-		
-		// Two raters, chance-corrected agreement.
-		TwoRaterSAgreement s = new TwoRaterSAgreement(study);
-		assertEquals(0.7, s.calculateObservedAgreement(), 0.001);
-		assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.4, s.calculateAgreement(), 0.001);
-		
-		TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
-		assertEquals(0.7, pi.calculateObservedAgreement(), 0.001);
-		assertEquals(0.545, pi.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.341, pi.calculateAgreement(), 0.001);
-		
-		TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
-		assertEquals(0.7, kappa.calculateObservedAgreement(), 0.001);
-		assertEquals(0.54, kappa.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.348, kappa.calculateAgreement(), 0.001);
-	}
-	
-	/***/
-	public void testArtsteinPoesio2008_2() {
-		IAnnotationStudy study = AgreementTestExamples.createArtsteinPoesio2008_2();
+    // public void testTwoRaterAgreementSingleItem()
+    // {
+    // final AnnotationStudy study = new AnnotationStudy(2);
+    // study.addItem("c", "c");
+    //
+    // final double expectedObservedAgreement = 1.0;
+    // final double actualObservedAgreement = new TwoRaterObservedAgreement(study)
+    // .calculateAgreement();
+    // Assert.assertEquals(expectedObservedAgreement, actualObservedAgreement, 1.0);
+    //
+    // final double expectedKappaAgreement = 1.0;
+    // final double actualKappaAgreement = new TwoRaterKappaAgreement(study).calculateAgreement();
+    // Assert.assertEquals(expectedKappaAgreement, actualKappaAgreement, 1.0);
+    // }
 
-		TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
-		assertEquals(0.88, raw.calculateAgreement(), 0.001);
-		
-		TwoRaterSAgreement s = new TwoRaterSAgreement(study);
-		assertEquals(0.88, s.calculateObservedAgreement(), 0.001);
-		assertEquals(0.333, s.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.82, s.calculateAgreement(), 0.001);
-		
-		TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
-		assertEquals(0.88, pi.calculateObservedAgreement(), 0.001);
-		assertEquals(0.401, pi.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.799, pi.calculateAgreement(), 0.001);
-		
-		TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
-		assertEquals(0.88, kappa.calculateObservedAgreement(), 0.001);
-		assertEquals(0.396, kappa.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.801, kappa.calculateAgreement(), 0.001);
-	}
+    /***/
+    public void testArtsteinPoesio2008_1()
+    {
+        final IAnnotationStudy study = AgreementTestExamples.createArtsteinPoesio2008_1();
 
-	/***/
-	public void testMeyer2009_1() {
-		AnnotationStudy study = AgreementTestExamples.createMeyer2009_1();
-						
-		TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
-		double agreement = raw.calculateAgreement();
-		double se = raw.standardError(agreement);
-		double[] ci = raw.confidenceInterval(agreement, se, TwoRaterObservedAgreement.CONFIDENCE_95);
-		assertEquals(0.7, agreement);
-		assertEquals(0.045, se, 0.001);
-		assertEquals(0.610, ci[0], 0.001);
-		assertEquals(0.789, ci[1], 0.001);	
-		
-		TwoRaterSAgreement s = new TwoRaterSAgreement(study);
-		assertEquals(0.7, s.calculateObservedAgreement(), 0.001);
-		assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.4, s.calculateAgreement(), 0.001);
-		
-		TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
-		assertEquals(0.7, pi.calculateObservedAgreement(), 0.001);
-		assertEquals(0.545, pi.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.341, pi.calculateAgreement(), 0.001);
-		
-		TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
-		assertEquals(0.7, kappa.calculateObservedAgreement(), 0.001);
-		assertEquals(0.54, kappa.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.348, kappa.calculateAgreement(), 0.001);
-	}
-	
-	/***/
-	public void testNumericalStability() {
-		IAnnotationStudy study = AgreementTestExamples.createNumericallyInstable();
-		
-		TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
-		assertEquals(0.9, raw.calculateAgreement(), 0.001);
-		
-		TwoRaterSAgreement s = new TwoRaterSAgreement(study);
-		assertEquals(0.9, s.calculateObservedAgreement(), 0.001);
-		assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.8, s.calculateAgreement(), 0.001);
-		
-		TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
-		assertEquals(0.9, pi.calculateObservedAgreement(), 0.001);
-		assertEquals(0.5, pi.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.8, pi.calculateAgreement(), 0.001);
-		
-		TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
-		assertEquals(0.9, kappa.calculateObservedAgreement(), 0.001);
-		assertEquals(0.5, kappa.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.8, kappa.calculateAgreement(), 0.001);
-		
-		
-		study = new AnnotationStudy(2);
-		for (int i = 0; i < 81001; i++)
-			study.addItem(1, 1);
-		for (int i = 0; i < 9000; i++)
-			study.addItem(1, 0);
-		for (int i = 0; i < 1000; i++)
-			study.addItem(0, 1);
-		for (int i = 0; i < 9000; i++)
-			study.addItem(0, 0);
-		
-		raw = new TwoRaterObservedAgreement(study);
-		assertEquals(0.9, raw.calculateAgreement(), 0.001);
-		
-		s = new TwoRaterSAgreement(study);
-		assertEquals(0.9, s.calculateObservedAgreement(), 0.001);
-		assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.8, s.calculateAgreement(), 0.001);
-		
-		pi = new TwoRaterPiAgreement(study);
-		assertEquals(0.9, pi.calculateObservedAgreement(), 0.001);
-		assertEquals(0.759, pi.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.585, pi.calculateAgreement(), 0.001);
-		
-		kappa = new TwoRaterKappaAgreement(study);
-		assertEquals(0.9, kappa.calculateObservedAgreement(), 0.001);
-		assertEquals(0.756, kappa.calculateExpectedAgreement(), 0.001);
-		assertEquals(0.590, kappa.calculateAgreement(), 0.001);
-	}
+        // Two raters, observed agreement.
+        final TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
+        assertEquals(0.7, raw.calculateAgreement(), 0.001);
+
+        final TwoRaterObservedCategoryAgreement cat = new TwoRaterObservedCategoryAgreement(study);
+        assertEquals(0.571, cat.calculateAgreement("STAT"), 0.001);
+        assertEquals(0.769, cat.calculateAgreement("IReq"), 0.001);
+
+        // Two raters, chance-corrected agreement.
+        final TwoRaterSAgreement s = new TwoRaterSAgreement(study);
+        assertEquals(0.7, s.calculateObservedAgreement(), 0.001);
+        assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.4, s.calculateAgreement(), 0.001);
+
+        final TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
+        assertEquals(0.7, pi.calculateObservedAgreement(), 0.001);
+        assertEquals(0.545, pi.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.341, pi.calculateAgreement(), 0.001);
+
+        final TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
+        assertEquals(0.7, kappa.calculateObservedAgreement(), 0.001);
+        assertEquals(0.54, kappa.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.348, kappa.calculateAgreement(), 0.001);
+    }
+
+    /***/
+    public void testArtsteinPoesio2008_2()
+    {
+        final IAnnotationStudy study = AgreementTestExamples.createArtsteinPoesio2008_2();
+
+        final TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
+        assertEquals(0.88, raw.calculateAgreement(), 0.001);
+
+        final TwoRaterSAgreement s = new TwoRaterSAgreement(study);
+        assertEquals(0.88, s.calculateObservedAgreement(), 0.001);
+        assertEquals(0.333, s.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.82, s.calculateAgreement(), 0.001);
+
+        final TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
+        assertEquals(0.88, pi.calculateObservedAgreement(), 0.001);
+        assertEquals(0.401, pi.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.799, pi.calculateAgreement(), 0.001);
+
+        final TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
+        assertEquals(0.88, kappa.calculateObservedAgreement(), 0.001);
+        assertEquals(0.396, kappa.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.801, kappa.calculateAgreement(), 0.001);
+    }
+
+    /***/
+    public void testMeyer2009_1()
+    {
+        final AnnotationStudy study = AgreementTestExamples.createMeyer2009_1();
+
+        final TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
+        final double agreement = raw.calculateAgreement();
+        final double se = raw.standardError(agreement);
+        final double[] ci = raw.confidenceInterval(agreement, se,
+                TwoRaterObservedAgreement.CONFIDENCE_95);
+        assertEquals(0.7, agreement);
+        assertEquals(0.045, se, 0.001);
+        assertEquals(0.610, ci[0], 0.001);
+        assertEquals(0.789, ci[1], 0.001);
+
+        final TwoRaterSAgreement s = new TwoRaterSAgreement(study);
+        assertEquals(0.7, s.calculateObservedAgreement(), 0.001);
+        assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.4, s.calculateAgreement(), 0.001);
+
+        final TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
+        assertEquals(0.7, pi.calculateObservedAgreement(), 0.001);
+        assertEquals(0.545, pi.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.341, pi.calculateAgreement(), 0.001);
+
+        final TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
+        assertEquals(0.7, kappa.calculateObservedAgreement(), 0.001);
+        assertEquals(0.54, kappa.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.348, kappa.calculateAgreement(), 0.001);
+    }
+
+    /***/
+    public void testNumericalStability()
+    {
+        IAnnotationStudy study = AgreementTestExamples.createNumericallyInstable();
+
+        TwoRaterObservedAgreement raw = new TwoRaterObservedAgreement(study);
+        assertEquals(0.9, raw.calculateAgreement(), 0.001);
+
+        TwoRaterSAgreement s = new TwoRaterSAgreement(study);
+        assertEquals(0.9, s.calculateObservedAgreement(), 0.001);
+        assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.8, s.calculateAgreement(), 0.001);
+
+        TwoRaterPiAgreement pi = new TwoRaterPiAgreement(study);
+        assertEquals(0.9, pi.calculateObservedAgreement(), 0.001);
+        assertEquals(0.5, pi.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.8, pi.calculateAgreement(), 0.001);
+
+        TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
+        assertEquals(0.9, kappa.calculateObservedAgreement(), 0.001);
+        assertEquals(0.5, kappa.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.8, kappa.calculateAgreement(), 0.001);
+
+        study = new AnnotationStudy(2);
+        for (int i = 0; i < 81001; i++) {
+            study.addItem(1, 1);
+        }
+        for (int i = 0; i < 9000; i++) {
+            study.addItem(1, 0);
+        }
+        for (int i = 0; i < 1000; i++) {
+            study.addItem(0, 1);
+        }
+        for (int i = 0; i < 9000; i++) {
+            study.addItem(0, 0);
+        }
+
+        raw = new TwoRaterObservedAgreement(study);
+        assertEquals(0.9, raw.calculateAgreement(), 0.001);
+
+        s = new TwoRaterSAgreement(study);
+        assertEquals(0.9, s.calculateObservedAgreement(), 0.001);
+        assertEquals(0.5, s.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.8, s.calculateAgreement(), 0.001);
+
+        pi = new TwoRaterPiAgreement(study);
+        assertEquals(0.9, pi.calculateObservedAgreement(), 0.001);
+        assertEquals(0.759, pi.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.585, pi.calculateAgreement(), 0.001);
+
+        kappa = new TwoRaterKappaAgreement(study);
+        assertEquals(0.9, kappa.calculateObservedAgreement(), 0.001);
+        assertEquals(0.756, kappa.calculateExpectedAgreement(), 0.001);
+        assertEquals(0.590, kappa.calculateAgreement(), 0.001);
+    }
 
 }
