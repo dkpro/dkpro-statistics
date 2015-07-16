40514c8355d38318f4aba57b7eebf5427a0657b4
Tristan Miller <psychonaut@nothingisreal.com>
2015-07-16 12:00:24 +0200

    Trim redundant code

--- dkpro-statistics-agreement/src/main/java/org/dkpro/statistics/agreement/unitizing/UnitizingAnnotationUnit.java
+++ dkpro-statistics-agreement/src/main/java/org/dkpro/statistics/agreement/unitizing/UnitizingAnnotationUnit.java
@@ -93,16 +93,6 @@ public class UnitizingAnnotationUnit extends AnnotationUnit
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		return super.equals(obj);
-	}
-
-	@Override
-	public int hashCode() {
-		return super.hashCode();
-	}
-
-	@Override
 	public String toString() {
 		return raterIdx + "<" + category + ">"
 				+ ((offset >= 0) ? "@" + offset : "")
