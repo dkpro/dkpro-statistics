99078ea441f155369f315e9454bd826da25cb8fc
Richard Eckart de Castilho <richard.eckart@gmail.com>
2026-07-11 18:59:51 +0200

    #58 - Alpha_U reports full agreement (1.0) for studies containing only zero-length units
    - Extract study information check into overridable `studyCarriesInformation()` method in `DisagreementMeasure`
    - Override `studyCarriesInformation()` in `UnitizingAgreementMeasure` to exclude zero-length units as degenerate (issue #35)
    - Add tests for zero-length-unit handling in `UnitizingAgreementTest`

--- dkpro-statistics-agreement/src/main/java/org/dkpro/statistics/agreement/unitizing/KrippendorffAlphaUnitizingAgreement.java
+++ dkpro-statistics-agreement/src/main/java/org/dkpro/statistics/agreement/unitizing/KrippendorffAlphaUnitizingAgreement.java
@@ -188,10 +188,6 @@ public class KrippendorffAlphaUnitizingAgreement
                     offset = pos;
                 }
 
-                if (length < 0) {
-                    var x = 0;
-                }
-
                 pos = offset + length;
             }
         }
